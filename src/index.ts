f2c8c73a58e45cc79b9f74bdba600b4e09fed75e
saltoriousSIG <cryptobet95@gmail.com>
2026-02-04 22:54:49 -0500

    ok

--- src/index.ts
+++ src/index.ts
@@ -226,9 +226,9 @@ function showHelp(): void {
   console.log(chalk.white('  jarvis skills             List available skills'));
   console.log(chalk.white('  jarvis help               Show this help\n'));
   console.log(chalk.gray('Examples:'));
-  console.log(chalk.gray('  bun run src/index.ts'));
-  console.log(chalk.gray('  bun run src/index.ts chat "What can you do?"'));
-  console.log(chalk.gray('  bun run src/index.ts init'));
+  console.log(chalk.gray('  npx tsx src/index.ts'));
+  console.log(chalk.gray('  npx tsx src/index.ts chat "What can you do?"'));
+  console.log(chalk.gray('  npx tsx src/index.ts init'));
 }
 
 /**
